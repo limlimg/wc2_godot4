7e9a5552a8edb7ad784fe1d67653683e085ecf63
limlimg <limlimg@sina.com>
2025-10-06 11:05:00 +0800

    expose global fonts in runtime_resource/font

--- app/src/main/cpp/ec_uni_font.gd
+++ app/src/main/cpp/ec_uni_font.gd
@@ -1,64 +1,58 @@
 extends Theme
 
-## THis class extends Theme because it hold both a font and a font size. The
+## This class extends Theme because it hold both a font and a font size. The
 ## .fnt files are imported as FontFile. @2x variant is not supported for the
 ## associated images.
 
+const _Profile = preload("res://app/src/main/cpp/runtime_resource/ec_uni_font_profile.gd")
 const _ecGraphics = preload("res://app/src/main/cpp/ec_graphics.gd")
 const _native = preload("res://app/src/main/cpp/native-lib.gd")
 
 @export
-var font_name: String:
-	set=set_font_name
+var profile: _Profile:
+	set=set_profile
 
-@export
-var font_name_hd: String:
-	set=set_font_name_hd
+var _last_selected_name: String
 
-@export
-var font_name_ipad: String:
-	set=set_font_name_ipad
-
-func set_font_name(value: String) -> void:
-		if value != font_name:
-			font_name = value
-			_changed()
-
-
-func set_font_name_hd(value: String) -> void:
-		if value != font_name_hd:
-			font_name_hd = value
-			_changed()
+func set_profile(value: _Profile):
+		if value != profile:
+			if profile != null:
+				profile.changed.disconnect(_profile_changed)
+			profile = value
+			if profile != null:
+				_profile_changed()
+				profile.changed.connect(_profile_changed)
+			else:
+				default_font = null
+				_last_selected_name = ""
 
 
-func set_font_name_ipad(value: String) -> void:
-		if value != font_name:
-			font_name = value
-			_changed()
-
-
-func _changed() -> void:
+func _profile_changed() -> void:
 	var selected_name: String
 	var is_hd := false
 	if not Engine.is_editor_hint():
-		if not font_name_ipad.is_empty() and _ecGraphics.instance().content_scale_size_mode == 3:
-			selected_name = font_name_ipad
-		elif not font_name_hd.is_empty() and _native.g_content_scale_factor == 2.0:
-			selected_name = font_name_hd
+		if not profile.name_ipad.is_empty() and _ecGraphics.instance().content_scale_size_mode == 3:
+			selected_name = profile.name_ipad
+		elif not profile.name_hd.is_empty() and _native.g_content_scale_factor == 2.0:
+			selected_name = profile.name_hd
 			is_hd = true
 		else:
-			selected_name = font_name
+			selected_name = profile.name
 	else:
-		selected_name = font_name
+		selected_name = profile.name
 	if selected_name.is_empty():
 		default_font = null
 		return
+	selected_name = selected_name.format([_native.g_localizable_strings.get_string("language")])
+	if _last_selected_name == selected_name:
+		return
+	_last_selected_name = selected_name
 	var selected_path := _native.get_path_alias(selected_name, "")
 	var font: FontFile
 	if not selected_path.is_empty():
 		font = load(selected_path) as FontFile
 	if font == null and Engine.is_editor_hint(): # in the editor, the situation is considered where only _hd variant exists
-		selected_path = _native.get_path_alias(font_name_hd, "")
+		selected_path = _native.get_path_alias(profile.name_hd, "")
 		if not selected_path.is_empty():
 			font = load(selected_path) as FontFile
 	if font == null:
@@ -73,10 +67,12 @@ func _changed() -> void:
 
 func init(file_name: String, hd: bool) -> void:
 	var old_font = default_font
+	var new_profile := _Profile.new()
 	if hd:
-		set_font_name_hd(file_name)
+		new_profile.name_hd = file_name
 	else:
-		set_font_name(file_name)
+		new_profile.name = file_name
+	set_profile(new_profile)
 	default_font.fallbacks.append(old_font)
 
 
