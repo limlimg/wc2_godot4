0150221b501e47ee763fd100c3ef9893c362a3d5
limlimg <limlimg@sina.com>
2026-01-15 17:37:39 +0800

    update resizing config

--- app/src/main/java/com/easytech/wc2/wc2_activity.gd
+++ app/src/main/java/com/easytech/wc2/wc2_activity.gd
@@ -103,11 +103,8 @@ func _show_game_view(_width: float, _height: float) -> void:
 	if _m_game_view_width > height_16_9:
 		_m_game_view_width = height_16_9
 		root.ready.connect(func():
+			root.content_scale_mode = Window.CONTENT_SCALE_MODE_CANVAS_ITEMS
 			root.content_scale_aspect = Window.CONTENT_SCALE_ASPECT_KEEP
-			, CONNECT_ONE_SHOT)
-	else:
-		root.ready.connect(func():
-			root.content_scale_aspect = Window.CONTENT_SCALE_ASPECT_IGNORE
 			, CONNECT_ONE_SHOT)
 	_ecRenderer.is_app_running = true
 	_m_gl_view = _ecGLSurfaceView.new()
