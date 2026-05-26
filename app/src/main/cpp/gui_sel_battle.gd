e64768d8a535057779cbc9744bab046163751162
limlimg <limlimg@sina.com>
2026-05-26 22:06:23 +0800

    feat(GUISelBattle): implement init of GUISelBattle

--- app/src/main/cpp/gui_sel_battle.gd
+++ app/src/main/cpp/gui_sel_battle.gd
@@ -1,8 +1,11 @@
-extends Control
+extends "res://app/src/main/cpp/gui_element.gd"
 
 const _GUIManager = preload("res://app/src/main/cpp/gui_manager.gd")
 const _ecTexture = preload("res://app/src/main/cpp/ec_texture.gd")
 const _ecImage = preload("res://app/src/main/cpp/ec_image.gd")
+const _native = preload("res://app/src/main/cpp/native-lib.gd")
+const _GUIBattleList = preload("res://app/src/main/cpp/gui_battle_list.gd")
+
 
 const _CAMPAIGN_LOGO = [
 	"res://app/src/main/cpp/scene_system_resource/selbattle_res/logo_axis.png.tres",
@@ -20,16 +23,27 @@ var game_mode: int:
 
 @export
 var campaign: int:
+	get():
+		return $GUIBattleList/GUIBattleList.campaign
 	set(value):
 		if value != campaign:
-			campaign = value
-			var logo: Texture2D = null
-			if value in _CAMPAIGN_LOGO:
-				logo = load(_CAMPAIGN_LOGO[value]) as Texture2D
-			$Logo/TextureRect.texture = logo
+			$GUIBattleList/GUIBattleList.campaign = value
+			if is_node_ready():
+				init()
 
 
-func init(init_game_mode: int, init_campaign: int) -> void:
-	game_mode = init_game_mode
-	campaign = init_campaign
-	_GUIManager.instance().s_texture_res = load("res://app/src/main/cpp/scene_system_resource/selbattle_res/texture_res.tres").get_res()
+func _ready() -> void:
+	init()
+
+
+func init() -> void:
+	var logo: Texture2D = null
+	if campaign < _CAMPAIGN_LOGO.size():
+		logo = load(_CAMPAIGN_LOGO[campaign]) as Texture2D
+	$Logo/TextureRect.texture = logo
+	_GUIElement.s_texture_res = load("res://app/src/main/cpp/scene_system_resource/selbattle_res/texture_res.tres").get_res()
+	var _list := $GUIBattleList/GUIBattleList as _GUIBattleList
+	if game_mode != 4 and g_Commander.get_num_played_battles(campaign) < _native.get_num_battles(campaign):
+		_list.select_last_unlocked()
+	else:
+		_list.set_select(0)
