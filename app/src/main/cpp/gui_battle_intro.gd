b1dc114cef7dc24f91dec07250b31b0fb22a43f7
limlimg <limlimg@sina.com>
2026-06-04 23:10:16 +0800

    feat(GUIBattleIntro): implement GUIBattleIntro

---
+++ app/src/main/cpp/gui_battle_intro.gd
@@ -0,0 +1,84 @@
+extends "res://app/src/main/cpp/gui_element.gd"
+
+const _native = preload("res://app/src/main/cpp/native-lib.gd")
+const _CObjectDef = preload("res://app/src/main/cpp/c_object_def.gd")
+
+
+const _BATTLE_INTRO_KEY_FORMAT = [
+	"axis battle intro %d",
+	"allies battle intro %d",
+	"wto battle intro %d",
+	"nato battle intro %d"
+]
+const _BATTLE_NAME_KEY_FORMAT = [
+	"axis battle name %d",
+	"allies battle name %d",
+	"wto battle name %d",
+	"nato battle name %d"
+]
+
+@export
+var campaign: int:
+	set(value):
+		if value != campaign:
+			campaign = value
+			if is_node_ready():
+				_set_battle()
+
+
+@export
+var battle: int:
+	set(value):
+		if value != battle:
+			battle = value
+			if is_node_ready():
+				_set_battle()
+
+
+signal ok_pressed
+
+func _ready() -> void:
+	init()
+	var graphics := _ecGraphics.instance()
+	_set_fade_color(graphics.fade_color)
+	graphics.fade_color_changed.connect(_set_fade_color)
+
+
+func init() -> void:
+	var battle_intro: Label = $BattleIntro/Control/Label
+	if _ecGraphics.instance().content_scale_size_mode == 3:
+		battle_intro.add_theme_constant_override("line_spacing", -6)
+	else:
+		battle_intro.add_theme_constant_override("line_spacing", -3)
+	$Victory/Control/Label.text = _native.g_string_table.get_string("victory")
+	$GreatVictory/Control/Label.text = _native.g_string_table.get_string("great victory")
+	_set_battle()
+
+
+func _set_battle() -> void:
+	var string_table := _native.g_string_table
+	if campaign >= 0 and campaign < 4:
+		$BattleIntro/Control/Label.text = string_table.get_string(_BATTLE_INTRO_KEY_FORMAT[campaign]%[battle + 1])
+		$BattleName/Control/Label.text = string_table.get_string(_BATTLE_NAME_KEY_FORMAT[campaign]%[battle + 1])
+	var battle_key_name := _native.get_battle_key_name(campaign, battle)
+	var battle_def := _CObjectDef.instance().get_battle_def(battle_key_name)
+	$Age/Control/Label.text = battle_def.age
+	var v1 := string_table.get_string("victory days1")
+	var v2 := string_table.get_string("victory days2")
+	var v := $VictoryDays/Control/Label
+	var gv := $GreatVictoryDays/Control/Label
+	if not v1.is_empty():
+		v.text = "%s %d %s"%[v1, battle_def.victory, v2]
+		gv.text = "%s %d %s"%[v1, battle_def.greatvictory, v2]
+	else:
+		v.text = "%d%s"%[battle_def.victory, v2]
+		gv.text = "%d%s"%[battle_def.greatvictory, v2]
+
+
+func _set_fade_color(color: Color) -> void:
+	color.a = 0.5
+	$ColorRect.color = color
+
+
+func _on_gui_button_ex_pressed() -> void:
+	ok_pressed.emit()
