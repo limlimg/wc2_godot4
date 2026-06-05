882c9676bd1707faf925ec7803bbf40ebbd6d455
limlimg <limlimg@sina.com>
2026-06-05 21:37:14 +0800

    feat(GUISelBattle): implement GUISelBattle

--- app/src/main/cpp/gui_sel_battle.gd
+++ app/src/main/cpp/gui_sel_battle.gd
@@ -36,12 +36,20 @@ var campaign: int:
 
 var _battle := -1
 var _center_pos: Vector2
+var _cur_tween_state := 0
+var _cur_tween: Tween
 @onready var _battle_list := $GUIBattleList/GUIBattleList
-@onready var _flag_proto := $Minimap/Elements/Flags/Flag
-@onready var _arrow_proto := $Minimap/Elements/Arrows/Arrow
+@onready var _flag_proto := $Minimap/ImageList/Flags/Flag
+@onready var _arrow_proto := $Minimap/ImageList/Arrows/Arrow
 @onready var _minimap: Control = $Minimap
 @onready var _gui_battle_intro: Control = $GUIBattleIntro
+@onready var _image_list: Node2D = $Minimap/ImageList
+@onready var _flags: Node2D = $Minimap/ImageList/Flags
+@onready var _age: CenterContainer = $Minimap/ImageList/Age
+@onready var _arrows: Node2D = $Minimap/ImageList/Arrows
 
+signal ok_pressed
+signal back_pressed
 
 func _ready() -> void:
 	init()
@@ -49,10 +57,10 @@ func _ready() -> void:
 
 func init() -> void:
 	if _ecGraphics.instance().content_scale_size_mode == 3:
-		$Minimap.scale = Vector2(2.0, 2.0)
+		_minimap.scale = Vector2(2.0, 2.0)
 		_flag_proto.scale = Vector2(0.5, 0.5)
 		_arrow_proto.scale = Vector2(0.5, 0.5)
-		$Minimap/Elements/CenterContainer.scale = Vector2(0.5, 0.5)
+		_age.scale = Vector2(0.5, 0.5)
 	var logo: Texture2D = null
 	if campaign < _CAMPAIGN_LOGO.size():
 		logo = load(_CAMPAIGN_LOGO[campaign]) as Texture2D
@@ -68,7 +76,24 @@ func _on_gui_battle_list_battle_selected(battle: int) -> void:
 	if _battle < 0:
 		_sel_battle(campaign, battle)
 		_minimap.position = _clamp_pos(_center_pos)
-		_battle = battle
+	else:
+		if _cur_tween_state != 1 and _cur_tween != null and _cur_tween.is_valid():
+			_cur_tween.kill()
+		_image_list.modulate.a = 1.0
+		_arrows.modulate.a = 1.0
+		_cur_tween_state = 1
+		_cur_tween = create_tween()
+		_cur_tween.tween_property(_image_list, "modulate", Color(_image_list.modulate, 0.0), 0.4)
+		_cur_tween.tween_callback(func ():
+			_arrows.modulate.a = 0.0
+			_sel_battle(campaign, _battle)
+			_cur_tween = create_tween()
+			var dest := _clamp_pos(_center_pos)
+			_cur_tween.tween_property(_minimap, "position", dest, dest.distance_to(_minimap.position) / 1000.0)
+			_cur_tween_state = 2
+			_cur_tween.tween_callback(_tween_fade_in_image)
+			)
+	_battle = battle
 
 
 func _sel_battle(sel_campaign: int, battle: int) -> void:
@@ -77,8 +102,8 @@ func _sel_battle(sel_campaign: int, battle: int) -> void:
 
 
 func _release_image_list() -> void:
-	_clear_element_nodes($Minimap/Elements/Flags, _flag_proto)
-	_clear_element_nodes($Minimap/Elements/Arrows, _arrow_proto)
+	_clear_element_nodes(_flags, _flag_proto)
+	_clear_element_nodes(_arrows, _arrow_proto)
 
 
 func _clear_element_nodes(parent: Node, keep: Node) -> void:
@@ -95,9 +120,9 @@ func _load_image_list(sel_campaign: int, battle: int) -> void:
 	if def == null:
 		return
 	_create_element_nodes(def.flag, "sflag_", _flag_proto)
-	_create_element_nodes(def.arrow, "maparrow_", $Minimap/Elements/Arrows/Arrow)
-	$Minimap/Elements/CenterContainer/Label.text = def.age
-	$Minimap/Elements/CenterContainer.position = Vector2(def.agex, def.agey)
+	_create_element_nodes(def.arrow, "maparrow_", _arrow_proto)
+	$Minimap/ImageList/Age/Label.text = def.age
+	_age.position = Vector2(def.agex, def.agey)
 	_center_pos = Vector2(def.centerx, def.centery)
 
 
@@ -133,8 +158,24 @@ func _clamp_pos(center_pos: Vector2) -> Vector2:
 	return -result
 
 
+func _tween_fade_in_image() -> void:
+	_cur_tween_state = 3
+	_cur_tween = create_tween()
+	_cur_tween.tween_property(_image_list, "modulate", Color(_image_list.modulate, 1.0), 0.5)
+	_cur_tween.tween_callback(func ():
+		_cur_tween_state = 4
+		)
+	_cur_tween.tween_property(_arrows, "modulate", Color(_arrows.modulate, 1.0), 1.0/1.5)
+	_cur_tween.tween_callback(func ():
+		_cur_tween_state = 0
+		)
+
+
 func _on_resized() -> void:
 	if is_node_ready():
+		if _cur_tween_state == 2:
+			_cur_tween.kill()
+			_tween_fade_in_image()
 		_minimap.position = _clamp_pos(_center_pos)
 
 
@@ -146,3 +187,12 @@ func _on_button_info_pressed() -> void:
 
 func _on_gui_battle_intro_ok_pressed() -> void:
 	_gui_battle_intro.hide()
+
+
+func _on_gui_button_ok_pressed() -> void:
+	g_GameManager.new_game(1, -1, campaign, _battle)
+	ok_pressed.emit()
+
+
+func _on_gui_button_back_pressed() -> void:
+	back_pressed.emit()
