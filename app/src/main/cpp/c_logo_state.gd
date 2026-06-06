6421d0cfa9f1c1af5919339bedb7500878ff89a9
limlimg <limlimg@sina.com>
2026-06-06 12:41:14 +0800

    refactor(CLogoState): use CBaseState callback

--- app/src/main/cpp/c_logo_state.gd
+++ app/src/main/cpp/c_logo_state.gd
@@ -1,21 +1,15 @@
-extends Control
+extends "res://app/src/main/cpp/c_base_state.gd"
 
+const _CSoundBox = preload("res://app/src/main/cpp/c_sound_box.gd")
 const _GUIManager = preload("res://app/src/main/cpp/gui_manager.gd")
 const _CStateManager = preload("res://app/src/main/cpp/c_state_manager.gd")
 
-class _CLogoState:
-	extends "res://app/src/main/cpp/native-lib.gd"
-	
-	static func on_enter() -> void:
-		g_GameSettings.load_settings()
-		var sound_box := _CSoundBox.get_instance()
-		sound_box.set_music_volume(g_GameSettings.music_volume)
-		sound_box.set_se_volume(g_GameSettings.se_volume)
-		_GUIManager.s_texture_res = load("res://app/src/main/cpp/scene_system_resource/logo_gui_res/texture_res.tres").get_res()
-
-
-func _ready() -> void:
-	_CLogoState.on_enter()
+func _on_enter() -> void:
+	g_GameSettings.load_settings()
+	var sound_box := _CSoundBox.get_instance()
+	sound_box.set_music_volume(g_GameSettings.music_volume)
+	sound_box.set_se_volume(g_GameSettings.se_volume)
+	_GUIManager.s_texture_res = load("res://app/src/main/cpp/scene_system_resource/logo_gui_res/texture_res.tres").get_res()
 	var tween := create_tween()
 	tween.tween_interval(2.1)
 	tween.tween_callback(func ():
