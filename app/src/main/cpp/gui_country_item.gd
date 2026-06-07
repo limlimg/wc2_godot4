69131cd08232250245afb0ab19e52b5e531b8cdc
limlimg <limlimg@sina.com>
2026-06-07 21:07:11 +0800

    feat(GUICountryItem): implement GUICountryItem

---
+++ app/src/main/cpp/gui_country_item.gd
@@ -0,0 +1,29 @@
+extends "res://app/src/main/cpp/gui_sel_item.gd"
+
+const _ecTextureResAssets = preload("res://app/src/main/cpp/scene_system_resource/ec_texture_res_assets.gd")
+
+@export
+var country_res: _ecTextureResAssets:
+	set(value):
+		if value != country_res:
+			country_res = value
+			if is_node_ready():
+				init()
+
+@export
+var country_name: String:
+	set(value):
+		if value != country_name:
+			country_name = value
+			if is_node_ready():
+				init()
+
+
+func init() -> void:
+	super()
+	if country_res == null:
+		return
+	var res := country_res.get_res()
+	var attr := res.get_image("button_%s.png"%(country_name))
+	if attr != null:
+		_button_image = _ecImage.new(attr)
